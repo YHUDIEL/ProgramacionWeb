d604b53943e6595da7801a0903698aa8b6bbccc9
Yhudiel Mendoza Sanchez <yhudielmendoza06@gmail.com>
2026-07-06 11:03:49 -0600

    Revert "ultimos cambios y mejoras"
    This reverts commit 548294ba61ca84f6d8cb37306f220de0890797cb.

--- js/ejercicio14.js
+++ js/ejercicio14.js
@@ -2,11 +2,6 @@ function calcular() {
     var entrada = document.getElementById('numeros').value;
     var numeros = entrada.split(',').map(Number);
 
-    if (numeros.some(isNaN)) {
-        alert('Por favor ingresa solo números separados por comas.');
-        return;
-    }
-
     document.getElementById('mayor').value = Math.max(...numeros);
     document.getElementById('menor').value = Math.min(...numeros);
 
